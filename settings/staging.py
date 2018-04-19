94a4d817bbe7e27a4c0d7b1601f59947080b9377
Justin Robinson <kilik2021@users.noreply.github.com>
2018-04-19 15:43:29 +0100

    Updated Staging.py to connect to JawsDB

--- settings/staging.py
+++ settings/staging.py
@@ -5,10 +5,11 @@ from base import *
 DEBUG = False
 
 DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	}
+	#'default': {
+	#	'ENGINE': 'django.db.backends.sqlite3',
+	#	'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	#}
+    'default': { dj_database_url.config('JAWSDB_URL')}
 }
 
 SITE_URL = 'https://boiling-spire-29247.herokuapp.com/'
